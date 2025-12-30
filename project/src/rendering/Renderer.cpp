02cdf4ab2dda0e5fbcc1ed9650c9a1dc4a0e9560
Kobe Dereyne <kobe.dereyne@outlook.com>
2025-12-30 19:48:19 +0100

    dont update settings in console every frame

--- project/src/rendering/Renderer.cpp
+++ project/src/rendering/Renderer.cpp
@@ -311,7 +311,7 @@ void ashen::Renderer::PrintStats()
         frames = 0;
         elapsedTime = 0.f;
     }
-
+    else return;
 
     // -- Move cursor up to overwrite previous stats --
     static bool first = true;
@@ -780,8 +780,6 @@ void ashen::Renderer::RenderFrame(uint32_t imageIndex)
 
 	SetRenderTarget(m_UseHDR ? renderImage.GetView() : m_pContext->GetSwapchainImageViews()[imageIndex], VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL);
     {
-        // -- Space Objects --
-
         // -- Ground Objects --
         Pipeline* pGroundShader = &m_GroundFromAtmosphere;
         pGroundShader->Bind(cmd);
