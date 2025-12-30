096153c1f4fcb032a25972f86310ebe1ec1298ea
Kobe Dereyne <kobe.dereyne@outlook.com>
2025-12-30 13:38:12 +0100

    remove variable

--- project/src/rendering/types/Types.h
+++ project/src/rendering/types/Types.h
@@ -45,7 +45,6 @@ namespace ashen
 		glm::vec3 lightDir;				// direction of the sunlight
 		float g;						// constant that affects symmetry of the scattering
 		float g2;						// g^2
-		uint32_t phaseType;				// Which phase function to use
 	};
 
 	// -- Ground --
