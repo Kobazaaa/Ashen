c90838dd75683818e6721242c43e308654625439
Kobe Dereyne <kobe.dereyne@outlook.com>
2026-01-17 14:04:07 +0100

    Update Readme

--- project/src/rendering/Renderer.cpp
+++ project/src/rendering/Renderer.cpp
@@ -292,9 +292,9 @@ void ashen::Renderer::HandleInput()
         m_PhaseFunctionIndex = (m_PhaseFunctionIndex + 1) % m_PhaseFunctionCount;
     fPrev = fCurr;
 
-#ifdef _DEBUG
+//#ifdef _DEBUG
     PrintStats();
-#endif
+//#endif
 }
 void ashen::Renderer::PrintStats()
 {
